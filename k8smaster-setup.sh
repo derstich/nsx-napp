8f0e5e11e4bd1992ea9eaa65480cec00dead455d
derstich <36949162+derstich@users.noreply.github.com>
2022-02-22 13:20:37 +0100

    Create k8smaster-setup.sh

---
+++ k8smaster-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export k8sfolder=~/k8sinstall
+export k8sversion=1.21.9
+export cpe=k8smaster
+export podnet=172.25.0.0/16
+
+mkdir $k8sfolder
+touch $k8sfolder/kubeadm-config.yaml
+printf \
+'apiVersion: kubeadm.k8s.io/v1beta2
+kind: ClusterConfiguration
+kubernetesVersion: '$k8sversion'
+controlPlaneEndpoint: "'$cpe':6443"
+networking:
+  podSubnet: '$podnet'' \
+| sudo tee -a $k8sfolder/kubeadm-config.yaml
+
+sudo kubeadm init --config=$k8sfolder/kubeadm-config.yaml --upload-certs | sudo tee $k8sfolder/kubeadm-init.out
