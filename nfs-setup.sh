cc28faa9b530983cd3b4463dacbac1f4c561eb68
derstich <36949162+derstich@users.noreply.github.com>
2022-02-22 17:06:34 +0100

    Update nfs-setup.sh

--- nfs-setup.sh
+++ nfs-setup.sh
@@ -8,11 +8,11 @@ export nfssubfolder=nfssubfolder
 #run commands
 sudo apt install nfs-kernel-server nfs-common -y
 echo -e 'n\np\n1\n\n\nw' | sudo fdisk /dev/$sdx
-sudo mkfs.ext4 /dev/$sdx'1'
+sudo mkfs.ext4 /dev/$sdx1
 sudo mkdir /$nfsfolder
-sudo mount /dev/$sdx'1' /$nfsfolder
-echo '/dev/'$sdx'1       /'$nfsfolder'    ext4    defaults     0   0' | sudo tee -a /etc/fstab
+sudo mount /dev/$sdx1 /$nfsfolder
+echo '/dev/$sdx1       /$nfsfolder    ext4    defaults     0   0' | sudo tee -a /etc/fstab
 sudo mkdir /$nfsfolder/$nfssubfolder -p
-echo '/'$nfsfolder'/'$nfssubfolder'               *(rw,sync,no_root_squash,no_subtree_check)' | sudo tee -a /etc/exports
+echo '/$nfsfolder/$nfssubfolder               *(rw,sync,no_root_squash,no_subtree_check)' | sudo tee -a /etc/exports
 sudo chown nobody:nogroup /$nfsfolder/$nfssubfolder/
 sudo systemctl restart nfs-kernel-server
