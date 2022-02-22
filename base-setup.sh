0ab91b4cfbf8cc1e6b942a655f22ca42857082b9
derstich <36949162+derstich@users.noreply.github.com>
2022-02-22 17:39:21 +0100

    Update base-setup.sh

--- base-setup.sh
+++ base-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #set Variables
-
+export installfiles=~/nappinstall
 #Hostnames
 export k8sm=k8smaster.corp.local
 export k8sn=k8snode.corp.local
@@ -13,7 +13,7 @@ export allhosts="$k8sm $k8sn $nfs"
 #Disk to use
 export sdx=sdb
 #Folder for NFS
-export nfsfolder=nfs 
+export nfsfolder=nfs
 export nfssubfolder=k8s
 
 #K8S Information
@@ -21,3 +21,13 @@ export k8sversion=1.21.9-00
 
 #LB IP Pool
 export iprange=192.168.110.81-192.168.110.90
+
+ssh $nfs mkdir $installfiles
+mkdir $installfiles
+curl -o $installfiles/nfs-setup.sh https://raw.githubusercontent.com/derstich/nsx-napp/main/nfs-setup.sh
+sed -i -e 's\$sdx\'$sdx'\g' $installfiles/nfs-setup.sh
+sed -i -e 's\$nfsfolder\'$nfsfolder'\g' $installfiles/nfs-setup.sh
+sed -i -e 's\$nfssubfolder\'$nfssubfolder'\g' $installfiles/nfs-setup.sh
+
+scp $installfiles/nfs-setup.sh $nfs:$installfiles/
+ssh $nfs bash $installfiles/nfs-setup.sh
