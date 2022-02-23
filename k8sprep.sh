1d6a7c2cab073ddf3fc1582040a8fbf317dff649
derstich <36949162+derstich@users.noreply.github.com>
2022-02-23 10:08:35 +0100

    Update k8sprep.sh

--- k8sprep.sh
+++ k8sprep.sh
@@ -2,12 +2,14 @@
 export iprange=192.168.110.81-192.168.110.90
 
 
-curl -o antrea.yaml https://raw.githubusercontent.com/antrea-io/antrea/main/build/yamls/antrea.yml
-kubectl apply -f antrea.yaml
+#Install CNI
+curl -o $k8sconfigfiles/antrea.yaml https://raw.githubusercontent.com/antrea-io/antrea/main/build/yamls/antrea.yml
+kubectl apply -f $k8sconfigfiles/antrea.yaml
+#Install LoadBalancer MetalB
 kubectl create ns metallb-system
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/main/manifests/metallb.yaml -n metallb-system
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-touch metallb-configmap.yaml
+touch $k8sconfigfiles/metallb-configmap.yaml
 printf \
 'apiVersion: v1
 kind: ConfigMap
@@ -20,9 +22,10 @@ data:
     - name: default
       protocol: layer2
       addresses:
-      - '$iprange'' \
-| tee -a metallb-configmap.yaml
-kubectl apply -f metallb-configmap.yaml
+      - $ippool' \
+| tee -a $k8sconfigfiles/metallb-configmap.yaml
+kubectl apply -f $k8sconfigfiles/metallb-configmap.yaml
+#Install NFS Provisioner
 sudo snap install helm --classic
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
 helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner --set nfs.server=nfs-k8s.corp.local --set nfs.path=/nfs/k8s --set storageClass.onDelete=true
