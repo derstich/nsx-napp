228c88e9967f06716a8e251d4d514a28cc6bda39
derstich <36949162+derstich@users.noreply.github.com>
2022-02-23 14:56:21 +0100

    Update base-setup.sh

--- base-setup.sh
+++ base-setup.sh
@@ -24,9 +24,17 @@ export k8sversion=1.21.9-00
 export pathdevmap=/dev/mapper/ubuntu--vg-ubuntu--lv
 export kubeadmfolder=~/kubeadm
 export podnet=172.25.0.0/16
+export k8sconfigfiles=~/k8sconfigfiles
 
 #LB IP Pool
-export iprange=192.168.110.81-192.168.110.90
+export ippool=192.168.110.81-192.168.110.90
+
+#NSX Manager Information
+export nsxmanager=192.168.110.7
+export nsxuser=admin
+export nsxpasswd='VMware1!VMware1!'
+export dockerregistry='projects.registry.vmware.com/nsx_application_platform/clustering'
+export helmrepo='https://projects.registry.vmware.com/chartrepo/nsx_application_platform'
 
 mkdir $installfiles
 for allhosts in $allhost; do
@@ -65,3 +73,20 @@ ssh $k8sm tail -n 2 $kubeadmfolder/kubeadm-init.out >> $installfiles/kubeadm-nod
 for k8snodes in $k8sn; do
 cat $installfiles/kubeadm-node.sh | ssh $k8snodes sudo -i
 done
+
+#Enable Management Host
+curl -o $installfiles/cli-setup.sh https://raw.githubusercontent.com/derstich/nsx-napp/main/cli-setup.sh
+sed -i -e 's\$k8sversion\'$k8sversion'\g' $installfiles/cli-setup.sh
+sed -i -e 's\$k8smaster\'$k8sm'\g' $installfiles/cli-setup.sh
+bash $installfiles/cli-setup.sh
+
+#Setup Loadbalancer MetalB
+curl -o $installfiles/k8sprep.sh https://raw.githubusercontent.com/derstich/nsx-napp/main/k8sprep.sh
+mkdir $k8sconfigfiles
+sed -i -e 's\$k8sconfigfiles\'$k8sconfigfiles'\g' $installfiles/k8sprep.sh
+sed -i -e 's\$ippool\'$ippool'\g' $installfiles/k8sprep.sh
+bash $installfiles/k8sprep.sh
+
+#Optional upload kube/config to nsx manager
+curl -k -u ''$nsxuser':'$nsxpasswd'' -H 'Accept:application/json' -d '{"docker_registry":"'$dockerregistry'","helm_repo":"'$helmrepo'"}' https://$nsxmanager/policy/api/v1/infra/sites/default/napp/deployment/registry
+curl -k -u ''$nsxuser':'$nsxpasswd'' -H 'Accept:application/json' -F 'file=@./.kube/config' https://$nsxmanager/policy/api/v1/infra/sites/default/napp/deployment/kubeconfig
