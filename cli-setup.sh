22de816bd5cd7730ec4ab15ab034149dad66574a
derstich <36949162+derstich@users.noreply.github.com>
2022-02-23 09:49:39 +0100

    Update cli-setup.sh

--- cli-setup.sh
+++ cli-setup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export k8sversion=1.21.9-00
-export k8sm=k8smaster.corp.local
-
 sudo apt-get update
 sudo apt-get upgrade -y
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
@@ -11,7 +8,7 @@ sudo apt-get update
 sudo apt-get install -y kubectl=$k8sversion
 sudo apt-mark hold kubectl
 mkdir -p $HOME/.kube
-scp $k8sm:.kube/config $HOME/.kube/config
+scp $k8smaster:.kube/config $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 source /usr/share/bash-completion/bash_completion
 echo 'source <(kubectl completion bash)' >>~/.bashrc
