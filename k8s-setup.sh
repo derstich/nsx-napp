07c9b823811477b5830d6b0ed1658dcbc0a60cf3
derstich <36949162+derstich@users.noreply.github.com>
2022-02-25 13:53:58 +0100

    Update k8s-setup.sh

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+#Swap must be disabled for Kubernetes
 sudo swapoff -a
 sudo sed -i -e 's\/swap.img\#/swap.img\g' /etc/fstab
+
+#Bring the System up to date
 sudo apt-get update
 sudo apt-get upgrade -y
+
+#Install Docker and chane the croupdrive to systemd
 sudo apt-get install -y docker.io
 sudo systemctl enable docker.service
 sudo systemctl start docker
@@ -18,12 +24,18 @@ sudo printf \
 sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo systemctl daemon-reload
 sudo systemctl restart docker
+
+#Install Kubernetes Packages
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo touch /etc/apt/sources.list.d/kubernetes.list
 sudo echo 'deb http://apt.kubernetes.io/  kubernetes-xenial  main' | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubeadm=$k8sversion kubelet=$k8sversion kubectl=$k8sversion
 sudo apt-mark hold kubelet kubeadm kubectl
+
+#For the NFS Provisioner we need to install the NfS Client on all Kubernetes Mashines
 sudo apt install nfs-client -y
+
+#To use the full Disksize for Kubernete we need to increase the logical Volume
 sudo lvextend -l +100%FREE $pathdevmap
 sudo resize2fs $pathdevmap
