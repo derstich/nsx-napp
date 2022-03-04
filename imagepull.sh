6a6848f2db106f50b64f1c53780939983f3c29ce
derstich <36949162+derstich@users.noreply.github.com>
2022-03-04 18:10:29 +0100

    Create imagepull.sh

---
+++ imagepull.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#CertManager Files
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/cert-manager-controller:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/cert-manager-cainjector:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/clients:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/cert-manager-webhook:19067763
+
+#Project Contour
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/contour:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/envoy:19067763
+
+#NSXI Platform
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/spark-operator:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/fluentd:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/minio:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/mc:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/druid:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/zookeeper:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/redis:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/cluster_api:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/postgresql-repmgr:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/routing_controller:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/fluent-bit:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/metrics_manager:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/clients:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/metrics_query_server:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/metrics-app:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/metrics_db_helper:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/third-party/clients:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/metrics_nsx_config:19067763
+
+#
+
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/hombre:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/nsx-cloud-connector-deployment-precheck:123-c33a1aa7.bionic
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/nsx-cloud-connector-ssl-tunnel:123-31bb14c6.bionic
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/sa-registration-scripts:19067767
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/nsx-cloud-connector-register-nsx-to-lastline-cloud:123-c33a1aa7.bionic
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/recommendation-spark-job:19067763
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/reputation-service:19067759
+sudo docker pull projects.registry.vmware.com/nsx_application_platform/clustering/nsx_config:19067763
